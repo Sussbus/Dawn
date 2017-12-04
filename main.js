be0add30aeef13838e417e2284398f46a64d89b9
Connor Larkin <connor.larkin1@gmail.com>
2017-12-03 18:56:09 -0600

    fix-path added

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const electron = require('electron')
 const ipc = require('electron').ipcMain;
-
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
 const path = require('path')
 const url = require('url')
+const fixPath = require('fix-path');
+
+fixPath();
+
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
